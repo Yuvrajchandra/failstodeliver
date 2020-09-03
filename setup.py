2f495f0ef12745f6a3467a6a972d442621eb8059
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2020-09-03 21:28:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="failstodeliver",
-    version="0.0.2",
+    version="0.0.3",
     license='MIT',
     author="Yuvraj Chandra",
     author_email="Singhyuvraj179@gmail.com",
     description="A simple package to download failstodeliver data from SEC EDGAR Databse",
-    long_description=long_description,
     url="https://github.com/Yuvrajchandra/failstodeliver",
-    download_url = 'https://github.com/Yuvrajchandra/failstodeliver/archive/v0.0.2.tar.gz',
+    download_url = 'https://github.com/Yuvrajchandra/failstodeliver/archive/v0.0.3.tar.gz',
     keywords = ['EDGAR', 'SEC', 'failstodeliver', 'Database'],
     packages=setuptools.find_packages(),
     classifiers=[
